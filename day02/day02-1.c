fde0495ca8d3464cfb5d06680797dd98e73415f8
Seongmin-Kim96 <smkim04150@gmail.com>
2024-09-26 13:19:53 +0900

    day02-1

--- day02/day02-1.c
+++ day02/day02-1.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
-
 int main()
 {
-	int operation;
-	printf("연산 타입: ");
-	scanf_s("%d", operation);
+	int ope;
+	printf("연산 타입:");
+	scanf_s("%d", &ope);
 
 	double num1, num2;
-	printf("첫번쨰 숫자:");
-	scanf_s("%lf", &num2);
-	printf("두 번쨰 숫자:");
+	printf("숫자 1을 입력하세요:");
+	scanf_s("%lf", &num1);
+
+	printf("숫자 2를 입력하세요:");
 	scanf_s("%lf", &num2);
 
 	double result;
-	if (operation == 1) {
+	result = 0;
+
+	if (ope == 1) {
+		result = num1 + num2;
+	}
+	else if (ope == 2) {
 		result = num1 - num2;
 	}
-	else if (operation == 2) {
-		result = num1 - num2;
+	else if (ope == 3) {
+		result = num1 * num2;
+	}
+	else if (ope == 4) {
+		result = num1 / num2;
 	}
 	else {
-		printf("잘못 입력했습니다.\n");
+		printf("잘못 입력하셨습니다\n");
 	}
-	printf("result=%lf" & num1);
 
-
+	printf("%lf*%lf=%lf", num1, num2, result);
 
 
 
